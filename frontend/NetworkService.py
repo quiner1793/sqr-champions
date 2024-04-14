31179db9905eba3cb995380bf235a108d32394bf
SHREDDING8 <zavrazhnov.zhora@mail.ru>
2024-04-14 18:55:18 +0300

    Profile complited

--- frontend/NetworkService.py
+++ frontend/NetworkService.py
@@ -1,4 +1,5 @@
 import requests
+from SessionService import *
 
 API_URL = "http://127.0.0.1:8080"
 
@@ -16,6 +17,17 @@ def register(email, username, password) -> (bool, str):
 def login(username, password):
     response = requests.post(f"{API_URL}/auth/token", data={"username": username, "password": password})
     if response.status_code == 200:
-        return True, response.json()['access_token'], response.json()["refresh_token"]
+        set_access_token(response.json()['access_token'])
+        set_refresh_token(response.json()["refresh_token"])
+
+        return True, ""
+
+    return False, response.json()["detail"]
+
+
+def get_user_info():
+    response = requests.get(f"{API_URL}/user/get_info", headers={"Authorization": f"Bearer {get_access_token()}"})
+    if response.status_code == 200:
+        return response.json()["success"], response.json()["user"]
 
     return False, response.json()["detail"]
