c9b2f509c9748506fb932b2ccee2b3db7e18e7ad
Anatoliy <shvarts.tolik@mail.ru>
2024-04-29 19:02:13 +0300

    Update according to flake8

--- tests/test_frontend/selenium/locators/base_page_locators.py
+++ tests/test_frontend/selenium/locators/base_page_locators.py
@@ -1,5 +1,6 @@
 from selene.support.shared.jquery_style import s
 
+
 class BasePageLocators:
     profile_page = s("/html/body/div/div[1]/div[1]/div/div/div/section[1]/div[1]/div[2]/ul/li[2]/div/a")
     comment_hub_page = s("/html/body/div/div[1]/div[1]/div/div/div/section[1]/div[1]/div[2]/ul/li[1]/div/a")
--- tests/test_frontend/selenium/locators/create_thread_locators.py
+++ tests/test_frontend/selenium/locators/create_thread_locators.py
@@ -1,9 +1,9 @@
 from selene.support.shared.jquery_style import s
 from locators.base_page_locators import BasePageLocators
 
+
 class CreateThreadLocators(BasePageLocators):
     url = s("//*[@id='text_input_2']")
     feedback = s("//*[@id='text_area_3']")
     create_btn = s("/html/body/div/div[1]/div[1]/div/div/div/section[2]/div[1]/div/div/div/div[5]/div/button")
     back = s("/html/body/div/div[1]/div[1]/div/div/div/section[2]/div[1]/div/div/div/div[1]/div/button")
-    
